c38a437f9bc09b6ebc7175d3d98f2f0320ca4c5d
Pavel Lysenko <code@ohar.name>
2018-11-18 19:11:21 +0100

    Build: add HTML generation

---
+++ deploy/generate-html.js
@@ -0,0 +1,35 @@
+const {author, name: title, description, repository, version} = require('./../package.json')
+const ejs                                                     = require('ejs')
+const fs                                                      = require('fs')
+const path                                                    = require('path')
+
+ejs.renderFile(
+  path.resolve(__dirname, './html_template.ejs'),
+  {
+    author,
+    description,
+    repository,
+    title,
+    version,
+  },
+  {},
+  (err, html) => {
+    if (err) {
+      console.error('HTML page generation failed', err)
+      throw err
+    } else {
+      fs.writeFile(
+        path.resolve(__dirname, './../dist/index.html'),
+        html,
+        err => {
+          if (err) {
+            console.error(`HTML page saving failed`, err)
+            throw err
+          } else {
+            console.log('HTML page is generated')
+          }
+        },
+      )
+    }
+  },
+)
